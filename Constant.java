76cbbdf8ce0857f5f7ba798c6a09c16b199d0738
junhoda <90564924+junhoda@users.noreply.github.com>
2021-09-29 16:05:22 +0900

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,19 @@
+
+public class Constant {
+
+	public static void main(String[] args) {
+		//variable : a, d, s
+		//constant : 1, 1.5, "Hello Java World"
+		int a = 1;
+		double d= 1.5;
+		String s = "Hello Java World";
+		a = a + 1;
+		d = d + 1;
+		s = "abcd";
+		
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(s);
+	}
+
+}
